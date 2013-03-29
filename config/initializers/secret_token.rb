2c467de0e7456603e9f4ffcd47ac504f313c5c57
Murat Alemdar <mr_frk@hotmail.com>
2013-03-29 11:43:46 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yeni::Application.config.secret_token = '00798dfe437ac19cb5681d24f509a742c897bd7bffb4dd07865f7d23898001ad2cce351a30bafad60866efd6eace33420aaa8a9bea12a793aa7add645a48ff29'
